e07dbaf181181bfb91a47a71920a1f0e4e92db7b
Yanzi Li <heymumuyang@gmail.com>
2019-02-27 12:45:35 +0800

    Initial Commit

---
+++ Interview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interview
+//
+//  Created by Yanzi Li  on 2019/2/27.
+//  Copyright © 2019 Yanzi Li . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
